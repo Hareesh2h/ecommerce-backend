d4d1b86d18979a7b17aec9e66436d01538daa399
Hareesh2h <63667815+Hareesh2h@users.noreply.github.com>
2025-07-15 21:27:25 +0530

    Delete src directory

--- src/main/java/com/chitpop/backend/repository/OrderItemRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chitpop.backend.repository;
-
-import com.chitpop.backend.entity.OrderItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
